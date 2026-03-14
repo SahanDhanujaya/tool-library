da0479104f66a22db2b1b3495a8bd29cb36b465d
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-15 01:02:47 +0530

    initial commit

---
+++ tool-inventory-service/src/test/java/com/dhanux/toolinventoryservice/ToolInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhanux.toolinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
